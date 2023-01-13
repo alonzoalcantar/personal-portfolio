fafc553f56e962331df0313133d8edaf77407017
alonzoalcantar <pinoa.03@outlook.com>
2023-01-12 20:42:45 -0700

    Created initial skills component

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react"
 import headerImg from '../assets/img/header-img.svg'
 
 export const Banner = () => {
-    const toRotate = ['Software Engineer', 'Web Developer', 'Always Learning'];
+    const toRotate = ['Software Engineer'];
     const [loopNumber, setLoopNumber] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
@@ -44,7 +44,7 @@ export const Banner = () => {
                 <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7}>
                         <span className='tagline'>Welcome to my Portfolio</span>
-                        <h1>{`Hi I am Alonzo Alcantar`}<span className='wrap'>{text}</span></h1>
+                        <h1>{`Hi I am Alonzo Alcantar - `} <span className='wrap'>{text}</span></h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                         <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25}/> </button>
                     </Col>
